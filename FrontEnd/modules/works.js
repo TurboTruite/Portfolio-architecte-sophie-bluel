c83ad277e6776a0ccba5cba7c1f7a7efc6082ffc
TurboTruite <ladouce.matthieu@gmail.com>
2023-03-08 22:36:09 +0100

    Fetch projets OK

---
+++ FrontEnd/modules/works.js
@@ -0,0 +1,19 @@
+export async function fetchWorks() {
+  const r = await fetch("http://localhost:5678/api/works");
+  if (!r.ok) {
+    throw new Error("Problème d'acccès serveur");
+  }
+  const works = await r.json();
+
+  for (let i = 0; i < works.length; i++) {
+    const gallery = document.querySelector(".gallery");
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    img.src = works[i].imageUrl;
+    const figCaption = document.createElement("figcaption");
+    figCaption.innerHTML = works[i].title;
+    gallery.appendChild(figure);
+    figure.appendChild(img);
+    figure.appendChild(figCaption);
+  }
+}

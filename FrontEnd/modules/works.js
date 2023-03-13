ef7990a45e4822ff8c1e5a49bceadf5eb8b23ae7
TurboTruite <ladouce.matthieu@gmail.com>
2023-03-13 16:23:16 +0100

    Modale v2

--- FrontEnd/modules/works.js
+++ FrontEnd/modules/works.js
@@ -21,24 +21,62 @@ function generateWorks(works) {
 
 generateWorks(works);
 
+
+// Générer la galerie dans la modale
+
 function generateModalGallery(works) {
   for (let i = 0; i < works.length; i++) {
     const gallery = document.querySelector(".modal-gallery");
-    console.log('on y est tkt frr')
     const figure = document.createElement("figure");
     const img = document.createElement("img");
     img.src = works[i].imageUrl;
+    img.setAttribute("class", "overlay-img")
     const editLink = document.createElement("a");
     editLink.innerHTML = "éditer";
+    const imgOverlay = document.createElement("div");
+    imgOverlay.setAttribute('class', 'image-overlay');
+    const trashOverlayButton = document.createElement("button");
+    trashOverlayButton.setAttribute('class', 'overlay-button');
+    const trashcan = document.createElement("i");
+    trashcan.setAttribute('class', 'fa-solid fa-trash-can');
+    const arrowsOverlayButton = document.createElement("button");
+    arrowsOverlayButton.setAttribute('class', 'overlay-button');
+    arrowsOverlayButton.setAttribute('display', 'block');
+    const arrows = document.createElement("i");
+    arrows.setAttribute('class', 'fa-solid fa-up-down-left-right');
     gallery.appendChild(figure);
     figure.appendChild(img);
+    figure.appendChild(imgOverlay);
+    imgOverlay.appendChild(trashOverlayButton);
+    trashOverlayButton.appendChild(trashcan);
+    imgOverlay.appendChild(arrowsOverlayButton);
+    arrowsOverlayButton.appendChild(arrows);
     figure.appendChild(editLink);
   }
 }
 
-generateModalGallery(works)
+generateModalGallery(works);
+
+// Modale : apparition des flèches de déplacement au hover
+
+function arrowsOnHover () {
+  const modalGalleryImg = document.querySelectorAll(".image-overlay");
+  for (let i = 0; i < modalGalleryImg.length; i++) {
+    modalGalleryImg[i].addEventListener("mouseover", () => {
+      console.log('youpi');
+    });
+  }
+
+}
+
+
+arrowsOnHover()
+
+
+
 
 // Filtrer projets : afficher tout
+
 const allButton = document.querySelector("#tous");
 
 allButton.addEventListener("click", function () {
